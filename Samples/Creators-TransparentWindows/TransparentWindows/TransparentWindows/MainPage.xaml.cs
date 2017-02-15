53f3cc9d0d349f40d92b3f91268ed450c960b832
Daren May <daren.may@gmail.com>
2017-02-14 21:35:55 -0700

    Adding a grey tint via a ColorMatrixEffect

--- Samples/Creators-TransparentWindows/TransparentWindows/TransparentWindows/MainPage.xaml.cs
+++ Samples/Creators-TransparentWindows/TransparentWindows/TransparentWindows/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using Windows.ApplicationModel.Core;
+using Windows.Graphics.Effects;
 using Windows.UI.Composition;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Media;
+using Microsoft.Graphics.Canvas.Effects;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -39,7 +41,43 @@ namespace TransparentWindows
             set
             {
                 _compositor = value;
-                CompositionBrush = _compositor.CreateHostBackdropBrush();
+
+                // adding a grey tint to the background
+                var colorMatrix = new Matrix5x4()
+                {
+                    M11 = 0.8f,
+                    M12 = 0f,
+                    M13 = 0f,
+                    M14 = 0,
+                    M21 = 0f,
+                    M22 = 0.8f,
+                    M23 = 0f,
+                    M24 = 0,
+                    M31 = 0f,
+                    M32 = 0f,
+                    M33 = 0.8f,
+                    M34 = 0,
+                    M41 = 0f,
+                    M42 = 0f,
+                    M43 = 0f,
+                    M44 = 1f,
+                    M51 = 0,
+                    M52 = 0,
+                    M53 = 0,
+                    M54 = 0
+                };
+
+                var graphicsEffect = new ColorMatrixEffect()
+                {
+                    ColorMatrix = colorMatrix,
+                    Source = new CompositionEffectSourceParameter("Background")
+                };
+
+                var effectFactory = _compositor.CreateEffectFactory(graphicsEffect, null);
+                var brush = effectFactory.CreateBrush();
+
+                brush.SetSourceParameter("Background", _compositor.CreateHostBackdropBrush());
+                CompositionBrush = brush;
             }
         }
     }
